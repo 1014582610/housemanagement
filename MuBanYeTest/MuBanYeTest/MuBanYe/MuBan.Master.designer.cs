6c6bd9dd9e0c8911687cd66f031e4a0531a6a815
wangyuesong <573978301@qq.com>
2013-07-20 23:37:43 +0800

    修改了Login

--- MuBanYeTest/MuBanYeTest/MuBanYe/MuBan.Master.designer.cs
+++ MuBanYeTest/MuBanYeTest/MuBanYe/MuBan.Master.designer.cs
@@ -31,6 +31,15 @@ namespace MuBanYeTest {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// ContentPlaceHolder2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+        
+        /// <summary>
         /// TextBox1 控件。
         /// </summary>
         /// <remarks>
